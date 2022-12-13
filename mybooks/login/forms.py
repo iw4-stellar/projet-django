55dc5d768e9bfebb513dd3014f12b2ecf6c9173d
Rudro Khan <rudrokhan.pro@gmail.com>
2022-12-13 16:57:43 +0100

    add of bookseller registration form

--- mybooks/login/forms.py
+++ mybooks/login/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
+class BooksellerRegistrationForm(forms.Form):
+  email = forms.EmailField(
+    label="Email",
+    widget=forms.EmailInput,
+  )
+  password = forms.CharField(
+    label="Password",
+    widget=forms.PasswordInput,
+    max_length=16,
+  )
+
 class ClientRegistrationForm(forms.Form):
   email = forms.EmailField(
     label="Email",
--- mybooks/login/views.py
+++ mybooks/login/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User, Group
-from .forms import ClientRegistrationForm
+from .forms import BooksellerRegistrationForm, ClientRegistrationForm
 # Create your views here.
 
 def loginView(request):
@@ -9,7 +9,33 @@ def loginView(request):
   return HttpResponse(render(request, 'login/login.html', context))
 
 def registerBooksellerView(request):
-  context = {}
+  if request.method == 'POST':
+    form = BooksellerRegistrationForm(request.POST)
+    if form.is_valid():
+      email = form.cleaned_data['email']
+      password = form.cleaned_data['password']
+
+      # Create user with email & password
+      user = User.objects.create_user(
+        username=email,
+        email=email,
+        password=password,
+      )
+
+      # Add user to clients group
+      bookseller_group, _ = Group.objects.get_or_create(name='bookseller')
+      user.groups.add(bookseller_group)
+
+      # Save changes
+      user.save()
+
+    return HttpResponseRedirect('/')
+
+  form = BooksellerRegistrationForm()
+
+  context = {
+    'form': form,
+  }
   return HttpResponse(render(request, 'login/register-bookseller.html', context))
 
 def registerClientView(request):
